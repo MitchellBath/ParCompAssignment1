16bfe977cc5d144d2f4a4b0f375e2bfd1dce6497
Mitch Bath <mbath@uncc.edu>
2021-09-04 00:30:42 -0400

    Prelim testing 5

--- prelim.c
+++ prelim.c
@@ -4,7 +4,7 @@
 int main () {
 	int HOST_NAME_MAX = 64;
   char hostname[HOST_NAME_MAX+1];
-  gethostname(hostname, HOST_NAME_MAX+1);
+  int gethostname(hostname, HOST_NAME_MAX+1);
   
   printf("Hostname: %s\n", hostname);
   return 0;
