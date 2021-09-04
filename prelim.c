199f5dfdb01ebbfb70a6b6d4a200abba582e6e46
Mitch Bath <mbath@uncc.edu>
2021-09-04 00:37:40 -0400

    10! double digit commits. still on prelim

--- prelim.c
+++ prelim.c
@@ -1,12 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <limits.h>
-#include <stdio.h>
-int main () {
-	//int HOST_NAME_MAX = 64;
-  size_t size = 255;
-  char* hostname[size];
-  gethostname(hostname, size);
-  
-  printf("Hostname: %s\n", hostname);
-  return 0;
+int main(void) {
+  char hostname[HOST_NAME_MAX + 1];
+  gethostname(hostname, HOST_NAME_MAX + 1);
+
+  printf("hostname: %s\n", hostname);
+
+  return EXIT_SUCCESS;
 }
