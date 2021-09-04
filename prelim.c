f408975c00972e315a4896164f837268a1e8e158
Mitch Bath <mbath@uncc.edu>
2021-09-04 00:36:12 -0400

    9 :(

--- prelim.c
+++ prelim.c
@@ -5,7 +5,7 @@ int main () {
 	//int HOST_NAME_MAX = 64;
   size_t size = 255;
   char* hostname[size];
-  int gethostname(hostname, size);
+  gethostname(hostname, size);
   
   printf("Hostname: %s\n", hostname);
   return 0;
