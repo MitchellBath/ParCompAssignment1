a75bb567b73cde929a3a4924e7b45c6493a54d40
Mitch Bath <mbath@uncc.edu>
2021-09-04 02:12:04 -0400

    a

--- main.c
+++ main.c
@@ -66,6 +66,7 @@ double calc_numerical_integration(int functionid, int a, int b, int n, int inten
         }
     }
         double integral;
+        summation += 1;
         integral = outside * summation;
         //printf("calc: %lf\n", integral); prints 0.0
         return integral;
