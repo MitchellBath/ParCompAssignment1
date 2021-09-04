eddedb9acb8b571c6790e41eb1be50927d4d90a9
Mitch Bath <mbath@uncc.edu>
2021-09-04 00:31:37 -0400

    Prelim testing 6

--- prelim.c
+++ prelim.c
@@ -2,8 +2,8 @@
 #include <limits.h>
 #include <stdio.h>
 int main () {
-	int HOST_NAME_MAX = 64;
-  char hostname[HOST_NAME_MAX+1];
+	//int HOST_NAME_MAX = 64;
+  char hostname[1024];
   int gethostname(hostname, HOST_NAME_MAX+1);
   
   printf("Hostname: %s\n", hostname);
